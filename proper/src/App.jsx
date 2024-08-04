03e210fab4ce563ac8800c5ae0346a958d1a8de0
MeisZy <zyrusnw@gmail.com>
2024-08-04 22:56:09 +0800

    file upload

---
+++ proper/src/App.jsx
@@ -0,0 +1,12 @@
+import Landing from './components/Landing.jsx'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Landing/>
+    </>
+  )
+}
+
+export default App
---
+++ proper/src/components/Landing.jsx
@@ -0,0 +1,19 @@
+import './Landing.css'
+import Panel from '../assets/panel.png'
+
+function Landing() {
+  return (
+    <>
+      <div className='poles'>
+        
+      </div>
+      <div className="cell">
+        <div className="hinge">
+          <img src ={Panel}></img>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Landing
