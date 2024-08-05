6f90da48cee237735d15dea1abc53f2c2b79b634
MeisZy <zyrusnw@gmail.com>
2024-08-05 23:24:21 +0800

    update: minor changes with panels and css naming

--- proper/src/components/Panels.jsx
+++ proper/src/components/Panels.jsx
@@ -1,20 +1,25 @@
 import './Panels.css';
 //import Panel from '../assets/panel.png';
-import Pole from '../assets/panel_pole.png';
+//import Pole from '../assets/panel_pole.png';
 
 function Panels() {
   return (
     <>
         <div className='proper'>
           <div className="cell">
-              <div >
-                <img src={Pole} className='panelpole'/>
-              </div>
+                <div className='board'>
+                  <p>Happy</p>
+                </div>
           </div>
           <div className="cell">
-              <div >
-                <img src={Pole} className='panelpole'/>
-              </div>
+                <div className='board'>
+                  <p>Birthday,</p>
+                </div>
+          </div>
+          <div className="cell">
+                <div className='board'>
+                  <p style={{color:'#4cb4f0'}}>(Name!)</p>
+                </div>
           </div>
         </div>
     </>
