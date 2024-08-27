44dbef543ad999696534bfd09479a404a86b368f
MeisZy <zyrusnw@gmail.com>
2024-08-27 11:25:16 +0800

    revert

--- proper/src/components/Panels.jsx
+++ proper/src/components/Panels.jsx
@@ -1,90 +1,90 @@
-import { useState, useEffect } from 'react';
-import './Panels.css';
-import CinnArrow from '../assets/cinnarrows.png';
-import Arrow from '../assets/arrowup.png';
+  import { useState, useEffect } from 'react';
+  import './Panels.css';
+  import CinnArrow from '../assets/cinnarrows.png';
+  import Arrow from '../assets/arrowup.png';
 
-function Panels() {
-  const [clickedItemIndex, setClickedItemIndex] = useState(null);
-  const [showSelections, setShowSelections] = useState(false);
-  const [showCinnArrow, setShowCinnArrow] = useState(false);
-  const [animateDisplayIndex, setAnimateDisplayIndex] = useState(null);
-  const [showContent, setShowContent] = useState(false); // New state
+  function Panels() {
+    const [clickedItemIndex, setClickedItemIndex] = useState(null);
+    const [showSelections, setShowSelections] = useState(false);
+    const [showCinnArrow, setShowCinnArrow] = useState(false);
+    const [animateDisplayIndex, setAnimateDisplayIndex] = useState(null);
+    const [showContent, setShowContent] = useState(false); // New state
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowCinnArrow(true);
-    }, 2500);
+    useEffect(() => {
+      const timer = setTimeout(() => {
+        setShowCinnArrow(true);
+      }, 2500);
 
-    return () => clearTimeout(timer);
-  }, []);
+      return () => clearTimeout(timer);
+    }, []);
 
-  const handleClick = (index) => {
-    setClickedItemIndex(index);
-    setAnimateDisplayIndex(index);
+    const handleClick = (index) => {
+      setClickedItemIndex(index);
+      setAnimateDisplayIndex(index);
 
-    setTimeout(() => {
-      setClickedItemIndex(null);
-      setAnimateDisplayIndex(null);
-    }, 5000); 
-  };
+      setTimeout(() => {
+        setClickedItemIndex(null);
+        setAnimateDisplayIndex(null);
+      }, 5000); 
+    };
 
-  const handleArrowClick = () => {
-    setShowSelections(true);
-  };
+    const handleArrowClick = () => {
+      setShowSelections(true);
+    };
 
-  const handleDisplayClick = () => {
-    setShowContent(prev => !prev); 
-  };
+    const handleDisplayClick = () => {
+      setShowContent(prev => !prev); 
+    };
 
-  return (
-    <div className='parent'>
-      <div className='cellproper'>
-        <div className='cells'>
-          <div className='board1'>
-            <p>Happy</p>
-          </div>
-          <div className='board2'>
-            <p>Birthday,</p>
-          </div>
-          <div className='board3'>
-            <p style={{ color: '#4cb4f0' }}>(name)</p>
+    return (
+      <>
+        <div className='cellproper'>
+          <div className='cells'>
+            <div className='board1'>
+              <p>Happy</p>
+            </div>
+            <div className='board2'>
+              <p>Birthday,</p>
+            </div>
+            <div className='board3'>
+              <p style={{ color: '#4cb4f0' }}>(name)</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className={`lowerguide ${showCinnArrow ? 'visible' : ''}`}>
-        <a href="#" onClick={handleArrowClick}>
-          <img src={CinnArrow} alt="CinnArrow"></img>
-        </a>
-      </div>
-      <div className={`selectionsproper ${showSelections ? 'visible' : ''}`}>
-        {[0].map(index => (
-          <a href="#"
-             key={index}
-             className={`items ${clickedItemIndex === index ? 'spin' : ''}`}
-             onClick={() => handleClick(index)}>
+        <div className={`lowerguide ${showCinnArrow ? 'visible' : ''}`}>
+          <a href="#" onClick={handleArrowClick}>
+            <img src={CinnArrow} alt="CinnArrow"></img>
           </a>
-        ))}
-      </div>
-      <div 
-        className={`displayproper ${showSelections ? 'visible' : ''}`} 
-        onClick={handleDisplayClick} 
-      >
-        {[0].map(index => (
-          <a href="#"
-             key={index}
-             className={`displayitems ${animateDisplayIndex === index ? 'animate' : ''}`}
-          >
-            <a>
-              <img src={Arrow} style={{border: '1px solid black'}}/>
+        </div>
+        <div className={`selectionsproper ${showSelections ? 'visible' : ''}`}>
+          {[0].map(index => (
+            <a href="#"
+              key={index}
+              className={`items ${clickedItemIndex === index ? 'spin' : ''}`}
+              onClick={() => handleClick(index)}>
             </a>
-          </a>
-        ))}
-      </div>
-        <div className={`contentproper ${showContent ? 'visible' : ''}`}> 
-          <div className='texts'>test</div>
+          ))}
         </div>
-    </div>
-  );
-}
+        <div 
+          className={`displayproper ${showSelections ? 'visible' : ''}`} 
+          onClick={handleDisplayClick} 
+        >
+          {[0].map(index => (
+            <a href="#"
+              key={index}
+              className={`displayitems ${animateDisplayIndex === index ? 'animate' : ''}`}
+            >
+              <a>
+                <img src={Arrow}/>
+              </a>
+            </a>
+          ))}
+        </div>
+          <div className={`contentproper ${showContent ? 'visible' : ''}`}> 
+            <div className='texts'>test</div>
+          </div>
+      </>
+    );
+  }
 
-export default Panels;
+  export default Panels;
